887da89ee57197ef2800980ce5f367520fdc66f5
549191130@qq.com <549191130@qq.com>
2019-06-01 13:30:52 +0800

    在dev分支上新增test包和TestController类

---
+++ src/com/ifx/test/TestController.java
@@ -0,0 +1,12 @@
+package com.ifx.test;
+
+/**
+ * @Author: djh
+ * @Date: 2019/6/1 0001
+ * @Description:
+ */
+public class TestController {
+    public void test() {
+        System.out.println("我只是测试使用...");
+    }
+}
